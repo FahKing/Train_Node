3f4a1628245b406585d953e543e7ce999aef2eb1
FahKing <fahkingnp@gmail.com>
2019-09-10 15:51:17 +0700

    add exports.login and jwt/config

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,10 +1,12 @@
 const User = require('../models/user')
 const {validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken')
+const config = require('../config/index')
 exports.register = async (req,res,next) => {
     
    try {
         const {name,email,password} = req.body
-        
+
         const errorValidation = validationResult(req)
         if(!errorValidation.isEmpty()){
             const error = new Error('Please input required information')
@@ -37,4 +39,41 @@ exports.register = async (req,res,next) => {
        next(error)
    }
 
+}
+
+exports.login = async (req,res,next) => {
+    try {
+       const {email,password} = req.body
+
+       const user = await User.findOne({email:email})
+       if(!user){
+           const error = new Error('Email not found')
+           error.statusCode = 401
+           throw error 
+       }
+
+       const validPassword = await user.validPassword(password)
+       if(!validPassword){
+           const error = new Error('Password or Email Incorrect')
+           error.statusCode = 401
+           throw error 
+       }
+       const token = jwt.sign({
+           id:user._id,
+           role: user.role
+        },config.JWT_SECRET,{expiresIn:'5 days'})
+
+
+       const  expires_in = jwt.decode(token)
+      
+       return res.json({
+           access_token: token,
+           expires_in: expires_in,
+           token_type: 'Bearer',
+           message: 'logined'
+       })
+
+    } catch (error) {
+        next(error)
+    }
 }
