d528cae40b03a8d9188f5a156667b1d83191d5ab
FahKing <fahkingnp@gmail.com>
2019-09-10 11:53:21 +0700

    modified errorHandler for Email uniq

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -2,18 +2,31 @@ const User = require('../models/user')
 
 exports.register = async (req,res,next) => {
     
-    const {name,email,password} = req.body
+   try {
+        const {name,email,password} = req.body
 
-    let user = new User
+        const exitMail = await User.findOne({email:email})
 
-    user.name = name
-    user.email = email
-    user.password = await user.encryptPassword(password)
+        if(exitMail){
+            const error = new Error("This Email is not Avalible")
+            error.statusCode = 403
+            throw error        
+        }
 
-    await user.save()
+        let user = new User
 
-    res.status(201).json({
-        data:user
-    })
+        user.name = name
+        user.email = email
+        user.password = await user.encryptPassword(password)
+
+        await user.save()
+
+        res.status(201).json({
+            data:user
+        })
+
+   } catch (error) {
+       next(error)
+   }
 
 }
