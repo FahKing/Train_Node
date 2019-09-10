1d806090a4bf5e0fcc2f7635fdf1fe51f38aca8b
FahKing <fahkingnp@gmail.com>
2019-09-10 13:54:11 +0700

    modied add required for name / password

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const usercontroller = require('../controllers/usercontroller')
+const {body} = require('express-validator')
 /* GET users listing. */
-router.post('/register', usercontroller.register);
+
+router.post('/register', 
+    body('name').not().isEmpty().withMessage('Please input required Name'),
+    body('password').not().isEmpty().withMessage('Please input required Password'),
+    usercontroller.register);
 
 module.exports = router;
