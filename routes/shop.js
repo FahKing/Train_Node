64db3ab52aabf116d631f74a2ae49a80f05d8854
FahKing <fahkingnp@gmail.com>
2019-09-11 13:59:02 +0700

    add routes

--- routes/shop.js
+++ routes/shop.js
@@ -2,5 +2,9 @@ const express = require('express');
 const router = express.Router();
 const shopController = require('../controllers/shopController')
 
-router.get('/',shopController.index)
+router.get('/',shopController.index);
+
+router.get('/menu', shopController.menu);
+
+router.get('/:id', shopController.getShopWithMenu);
 module.exports = router;
